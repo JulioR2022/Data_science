ea9cfbb2e6f188260c10c8ef56cda06551cf3a3a
JulioR2022 <221022041@aluno.unb.br>
2025-05-15 12:12:49 -0300

    Given a normal distribution N(mean, std), the lower tail area for an observation X is calculated, and the curve is plotted.

---
+++ exercises-OS/guide4-9.py
@@ -0,0 +1,71 @@
+from scipy.stats import norm
+import numpy as np
+import matplotlib.pyplot as plt
+# This program calculates the tail area of a normal distribution given its mean and standard deviation.
+# It uses the z-score to find the percentile of a given value.
+# The program also plots the normal distribution curve with the tail area shaded.
+
+def z_score(x,mean, std):
+    return (x - mean) / std
+
+def percentile(z):
+    return norm.cdf(z)
+
+def nomal_curve_with_tail(z, media= 0, std= 1):
+    x = np.linspace(media - 4 * std, media + 4 * std, 1000)
+    y = norm.pdf(x, media, std)
+
+    plt.plot(x, y, label='Normal Distribution')
+    plt.title('Normal Distribution with Tail Area')
+    plt.xlabel('Observations')
+    plt.ylabel('Density')
+    x_fill = np.linspace(media - 4*std, media + z * std, 1000)
+    y_fill = norm.pdf(x_fill, media, std)
+    plt.fill_between(x_fill, y_fill, alpha=0.5, label='Tail Area',color='green')
+
+    plt.axvline(x=media + z * std, color='red', linestyle='--', label='Z-Score')
+    plt.legend()  
+    plt.grid(True)
+    plt.show()
+
+
+message = 'Enter the mean and the standard deviation of the normal distribution: '
+mean, std = map(int,input(message).split())
+
+while True:
+    entry = input('Enter a value to calculate the percentile (or "exit" to quit): ')
+    if entry.lower() == 'exit':
+        print('Exiting the program.')
+        break
+
+    entry = float(entry)
+    z = z_score(entry, mean, std)
+    p = percentile(z)
+    print(f'The tail area for {entry} is: {p:.4f}')
+    nomal_curve_with_tail(z, mean, std)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
